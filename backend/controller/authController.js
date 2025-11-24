e32099b8cf9a3510bdd142a6535cd75a09d2e010
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2025-11-24 15:44:55 +0530

    add url

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -36,8 +36,8 @@ export const register = async (req, res) => {
 
     res.cookie('token', token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "none",
         masAge: 7 * 24 * 60 * 60 * 1000
     })
     return res.status(201).json({success: true})
@@ -71,8 +71,8 @@ export const login = async (req, res) => {
 
     res.cookie('token', token, {
         httpOnly: true,
-        secure: false,
-        sameSite: "strict",
+        secure: process.env.NODE_ENV === "production",
+        sameSite: "none",
         masAge: 7 * 24 * 60 * 60 * 1000
     })
     return res.status(201).json({success: true})
