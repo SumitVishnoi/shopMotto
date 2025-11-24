32c80700884397409243a769e6728a96c4662c8a
Sumit Vishnoi <sumitvishnoi7014@gmail.com>
2025-11-24 15:54:24 +0530

    add deploy url

--- backend/index.js
+++ backend/index.js
@@ -21,7 +21,7 @@ connectedDB()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174", "https://shopmotto.netlify.app"],
     credentials: true
 }))
 
